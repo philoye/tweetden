7fe74a030a6c9682d3f9937b15bc9bef6893e4d4
Phil Oye <philoye@philoye.com>
2012-11-18 16:57:02 +1100

    Don't need our home-brew partial implementation.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -37,22 +37,6 @@ def tweet_status_url(tweet_id,screen_name = ENV['TWEETDEN_SCREEN_NAME'])
   "http://twitter.com/#{screen_name}/status/#{tweet_id}"
 end
 
-# Stolen from http://gist.github.com/119874
-def partial(template, *args)
-  template_array = template.to_s.split('/')
-  template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
-  options = args.last.is_a?(Hash) ? args.pop : {}
-  options.merge!(:layout => false)
-  if collection = options.delete(:collection) then
-    collection.inject([]) do |buffer, member|
-      buffer << haml(:"#{template}", options.merge(:layout =>
-      false, :locals => {template_array[-1].to_sym => member}))
-    end.join("\n")
-  else
-    haml(:"#{template}", options)
-  end
-end
-
 # http://huri.net/tech/renaming-hash-keys
 class Hash
   def rename_key(old,new)
