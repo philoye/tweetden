eea4cc60841b11ec8efa9f0e6aabe108504e1206
Phil Oye <philoye@philoye.com>
2013-11-28 20:59:29 +1100

    Updated to use oauth and newest twitter api.

--- models/twitter_com.rb
+++ models/twitter_com.rb
@@ -1,13 +1,29 @@
 class TwitterCom
 
   def self.getTweets(page, count)
-    f = open("http://api.twitter.com/1/statuses/user_timeline/#{ENV['SCREEN_NAME']}.json?count=#{count}&include_rts=true&include_entities=true&page=#{page}").read
-    return JSON.parse(f)
+    token = prepare_access_token
+    response = token.request(:get, "https://api.twitter.com/1.1/statuses/user_timeline/#{ENV['SCREEN_NAME']}.json?count=#{count}&include_rts=true&include_entities=true&page=#{page}")
+    return JSON.parse(response.body)
   end
 
   def self.getUser(user)
-    f = open("http://api.twitter.com/1/users/show.json?screen_name=#{user}").read
-    return JSON.parse(f)
+    token = prepare_access_token
+    response = token.request(:get, "https://api.twitter.com/1.1/users/show.json?screen_name=#{user}")
+    return JSON.parse(response.body)
+  end
+
+  private
+
+  def self.prepare_access_token
+    consumer = OAuth::Consumer.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'],
+      { :site => "http://api.twitter.com",
+        :scheme => :header
+      })
+    token_hash = { :oauth_token => ENV['ACCESS_TOKEN'],
+                   :oauth_token_secret => ENV['ACCESS_TOKEN_SECRET']
+                 }
+    access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+    return access_token
   end
 
 end
